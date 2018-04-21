515fc588d9e01288a89cd01962e66d62f1c7712b
KARLA VANESSA BALLESTERO CASTRO <karla_vanesa_21@hotmail.com>
2018-04-21 17:04:18 -0600

    FINISH

--- FrmExamen/FrmPronosticoCiudades.Designer.cs
+++ FrmExamen/FrmPronosticoCiudades.Designer.cs
@@ -30,28 +30,31 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPronosticoCiudades));
             this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pbxMadrugada = new System.Windows.Forms.PictureBox();
             this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.bunifuDropdown1 = new Bunifu.Framework.UI.BunifuDropdown();
+            this.rtxtMessage = new System.Windows.Forms.RichTextBox();
+            this.cbx = new System.Windows.Forms.ComboBox();
+            this.pbxNoche = new System.Windows.Forms.PictureBox();
+            this.pbxTarde = new System.Windows.Forms.PictureBox();
+            this.pbxManana = new System.Windows.Forms.PictureBox();
             this.bunifuGradientPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMadrugada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxNoche)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTarde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxManana)).BeginInit();
             this.SuspendLayout();
             // 
             // bunifuGradientPanel1
             // 
             this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
             this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.bunifuGradientPanel1.Controls.Add(this.bunifuDropdown1);
-            this.bunifuGradientPanel1.Controls.Add(this.richTextBox1);
+            this.bunifuGradientPanel1.Controls.Add(this.pbxManana);
+            this.bunifuGradientPanel1.Controls.Add(this.pbxTarde);
+            this.bunifuGradientPanel1.Controls.Add(this.pbxNoche);
+            this.bunifuGradientPanel1.Controls.Add(this.cbx);
+            this.bunifuGradientPanel1.Controls.Add(this.rtxtMessage);
             this.bunifuGradientPanel1.Controls.Add(this.bunifuCustomLabel1);
-            this.bunifuGradientPanel1.Controls.Add(this.pictureBox3);
-            this.bunifuGradientPanel1.Controls.Add(this.pictureBox2);
-            this.bunifuGradientPanel1.Controls.Add(this.pictureBox1);
+            this.bunifuGradientPanel1.Controls.Add(this.pbxMadrugada);
             this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.White;
             this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.White;
             this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.White;
@@ -62,29 +65,15 @@
             this.bunifuGradientPanel1.Size = new System.Drawing.Size(744, 467);
             this.bunifuGradientPanel1.TabIndex = 0;
             // 
-            // pictureBox1
+            // pbxMadrugada
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(301, 373);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(497, 373);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(118, 373);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.pbxMadrugada.BackColor = System.Drawing.Color.Transparent;
+            this.pbxMadrugada.Location = new System.Drawing.Point(81, 342);
+            this.pbxMadrugada.Name = "pbxMadrugada";
+            this.pbxMadrugada.Size = new System.Drawing.Size(100, 111);
+            this.pbxMadrugada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxMadrugada.TabIndex = 2;
+            this.pbxMadrugada.TabStop = false;
             // 
             // bunifuCustomLabel1
             // 
@@ -95,29 +84,52 @@
             this.bunifuCustomLabel1.TabIndex = 3;
             this.bunifuCustomLabel1.Text = "Seleccione Ciudad: ";
             // 
-            // richTextBox1
+            // rtxtMessage
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(169, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(390, 175);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
+            this.rtxtMessage.Location = new System.Drawing.Point(169, 12);
+            this.rtxtMessage.Name = "rtxtMessage";
+            this.rtxtMessage.Size = new System.Drawing.Size(390, 175);
+            this.rtxtMessage.TabIndex = 4;
+            this.rtxtMessage.Text = "";
             // 
-            // bunifuDropdown1
+            // cbx
             // 
-            this.bunifuDropdown1.BackColor = System.Drawing.Color.Transparent;
-            this.bunifuDropdown1.BorderRadius = 3;
-            this.bunifuDropdown1.ForeColor = System.Drawing.Color.White;
-            this.bunifuDropdown1.Items = new string[0];
-            this.bunifuDropdown1.Location = new System.Drawing.Point(217, 292);
-            this.bunifuDropdown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.bunifuDropdown1.Name = "bunifuDropdown1";
-            this.bunifuDropdown1.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
-            this.bunifuDropdown1.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
-            this.bunifuDropdown1.selectedIndex = -1;
-            this.bunifuDropdown1.Size = new System.Drawing.Size(315, 39);
-            this.bunifuDropdown1.TabIndex = 5;
-            this.bunifuDropdown1.onItemSelected += new System.EventHandler(this.bunifuDropdown1_onItemSelected);
+            this.cbx.FormattingEnabled = true;
+            this.cbx.Location = new System.Drawing.Point(240, 294);
+            this.cbx.Name = "cbx";
+            this.cbx.Size = new System.Drawing.Size(261, 24);
+            this.cbx.TabIndex = 5;
+            this.cbx.SelectedIndexChanged += new System.EventHandler(this.cbx_SelectedIndexChanged);
+            // 
+            // pbxNoche
+            // 
+            this.pbxNoche.BackColor = System.Drawing.Color.Transparent;
+            this.pbxNoche.Location = new System.Drawing.Point(576, 342);
+            this.pbxNoche.Name = "pbxNoche";
+            this.pbxNoche.Size = new System.Drawing.Size(100, 111);
+            this.pbxNoche.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxNoche.TabIndex = 6;
+            this.pbxNoche.TabStop = false;
+            // 
+            // pbxTarde
+            // 
+            this.pbxTarde.BackColor = System.Drawing.Color.Transparent;
+            this.pbxTarde.Location = new System.Drawing.Point(417, 342);
+            this.pbxTarde.Name = "pbxTarde";
+            this.pbxTarde.Size = new System.Drawing.Size(100, 111);
+            this.pbxTarde.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxTarde.TabIndex = 7;
+            this.pbxTarde.TabStop = false;
+            // 
+            // pbxManana
+            // 
+            this.pbxManana.BackColor = System.Drawing.Color.Transparent;
+            this.pbxManana.Location = new System.Drawing.Point(240, 342);
+            this.pbxManana.Name = "pbxManana";
+            this.pbxManana.Size = new System.Drawing.Size(100, 111);
+            this.pbxManana.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxManana.TabIndex = 8;
+            this.pbxManana.TabStop = false;
             // 
             // FrmPronosticoCiudades
             // 
@@ -130,9 +142,10 @@
             this.Load += new System.EventHandler(this.PronosticoCiudades_Load);
             this.bunifuGradientPanel1.ResumeLayout(false);
             this.bunifuGradientPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMadrugada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxNoche)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTarde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxManana)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -140,12 +153,13 @@
         #endregion
 
         private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
-        private Bunifu.Framework.UI.BunifuDropdown bunifuDropdown1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtxtMessage;
         private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbxMadrugada;
+        private System.Windows.Forms.ComboBox cbx;
+        private System.Windows.Forms.PictureBox pbxManana;
+        private System.Windows.Forms.PictureBox pbxTarde;
+        private System.Windows.Forms.PictureBox pbxNoche;
     }
 }
 
--- FrmExamen/FrmPronosticoCiudades.cs
+++ FrmExamen/FrmPronosticoCiudades.cs
@@ -35,12 +35,56 @@ namespace FrmExamen
             //inicializar
             bo = new PronosticoCiudadesBO();
            list = bo.PronosticoCiudades();
-            MessageBox.Show(list.Count.ToString());
+
+            LlenarCbx();
+            cbx.SelectedIndex = 9;
         }
 
-        private void bunifuDropdown1_onItemSelected(object sender, EventArgs e)
+        public void LlenarCbx()
         {
-          
+            foreach (PRONOSTICO_PORCIUDADESCIUDAD ciudad in list)
+            {
+                cbx.Items.Add(ciudad.nombre);
+               
+            }
+
+        }
+
+        private void cbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rtxtMessage.Text = "";
+            string ciudad = cbx.SelectedItem.ToString();
+            foreach (PRONOSTICO_PORCIUDADESCIUDAD c in list)
+            {
+                if (c.nombre.Equals(ciudad))
+
+                {
+                    //if (c.ESTADOMADRUGADA != null)
+                    //{
+                    //    rtxtMessage.Text += Environment.NewLine + "El pronóstico para esta Madrugada es: " + c.ESTADOMADRUGADA.Value;
+                    //pbxMadrugada.ImageLocation = String.Format("https://www.imn.ac.cr{0}", c.ESTADOMADRUGADA.imgPath);
+                    //}
+                    //if (c.ESTADOMANANA != null)
+                    //{
+                    //    rtxtMessage.Text += Environment.NewLine + "El pronóstico para esta Mañana es: " + c.ESTADOMANANA.Value;
+                    //   pbxManana.ImageLocation = String.Format("https://www.imn.ac.cr{0}", c.ESTADOMANANA.imgPath);
+                    //}
+                    if (c.ESTADOTARDE != null)
+                    {
+                       rtxtMessage.Text += Environment.NewLine + "El pronóstico para esta Tarde es: " +c.ESTADOTARDE.Value;
+                        pbxTarde.ImageLocation = String.Format("https://www.imn.ac.cr{0}", c.ESTADOTARDE.imgPath);
+                    }
+                   if(c.ESTADONOCHE != null)
+                    {
+                        rtxtMessage.Text += Environment.NewLine + "El pronóstico para esta Noche es: " + c.ESTADONOCHE.Value;
+                        pbxNoche.ImageLocation = String.Format("https://www.imn.ac.cr{0}", c.ESTADONOCHE.imgPath);
+                    }
+
+
+
+                }
+
+            }
         }
     }
 }
