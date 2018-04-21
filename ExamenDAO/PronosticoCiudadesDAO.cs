1e6b05147eec6c4eac2966395c492e73240da174
KARLA VANESSA BALLESTERO CASTRO <karla_vanesa_21@hotmail.com>
2018-04-21 13:28:14 -0600

    Changes

---
+++ ExamenDAO/PronosticoCiudadesDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamenDAO.WSM;
+using Examen;
+
+namespace ExamenDAO  
+{
+
+    public  class PronosticoCiudadesDAO
+    {
+        private WSMeteorologicoClient ws;
+
+        public PronosticoCiudadesDAO()
+        {
+            ws = new WSMeteorologicoClient("WSMeteorologico");
+        }   
+    }
+}
---
+++ FrmExamen/FrmPronosticoCiudades.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace FrmExamen
+{
+    partial class FrmPronosticoCiudades
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmPronosticoCiudades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 465);
+            this.Name = "FrmPronosticoCiudades";
+            this.Text = "FrmPronosticoCiudades";
+            this.Load += new System.EventHandler(this.PronosticoCiudades_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
